2c4fac8ea1999e2924e49364d49ede65e352be3f
Elsewin-Lim <elsewin168@gmail.com>
2026-02-24 15:27:32 +0700

    Didn't change much just more comments
    Just like the old commit there's no changes in code, I've just made it easier for muyheang to use the UpdateScore function

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
-	import { UpdateScore } from '$lib/components/updateScore';
+	// import { UpdateScore } from '$lib/components/updateScore';
+
 	// Just call doUpdate(name, score) you don't need to do anything else
 	// if the user is first time, then they will be register in the database
 	// if the user already register, they will update their score instead
 	// I made the UpdateScore function to store their name and score in the cookie
 
-	function doUpdate(id: string, score: number) {
-		UpdateScore(id, score);
-	}
+	// function doUpdate(id: string, score: number) {
+	// 	UpdateScore(id, score);
+	// }
+
 	// Uncomment below to get the name and score in the cookies
 	// const {data} = $props();
 	// const name = data.name
