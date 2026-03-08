8093c31d2c188168429eec0592bcc92263b28bdc
Elsewin-Lim <elsewin168@gmail.com>
2026-03-08 21:15:47 +0700

    Helper function to het userSessionData created
    I've made a helper function in lib/server to get data from users cookies

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,2 @@
-import { stopGameBgm } from './components/gameBgm'
-
 // place files you want to import through the `$lib` alias in this folder.
 export { getUserSessionData } from './server/userSessionData'
--- src/lib/server/userSessionData.ts
+++ src/lib/server/userSessionData.ts
@@ -1,6 +1,13 @@
 import type { RequestEvent } from '@sveltejs/kit'
 
-export function getUserSessionData(event: RequestEvent) {
+type UserSession = {
+	id: string | number | undefined
+	name: string | undefined
+	score: number
+	registered_date: Date
+}
+
+export function getUserSessionData(event: RequestEvent): UserSession {
 	return {
 		// These datas will be sent to the +page.svelte page in the same folder
 		// Call it by using $props
